6480bdfea4a162f02285cbb976508955dc2b801c
Gerardo Merida <cgemerida@gmail.com>
2019-01-28 19:40:01 -0600

    Elimina el boton registrarse

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,9 +42,6 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">Iniciar Sesión</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">Registrar</a>
-                        </li>
                     @endauth
                 </ul>
             @endif
