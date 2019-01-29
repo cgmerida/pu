f40f5f0961375430a7b5105f473878b51e3b9acc
Gerardo Merida <gmeridarn@gmail.com>
2019-01-29 17:36:07 -0600

    Arregla el mapa responsive

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -3,10 +3,11 @@
 @section('css')
     <style>
         #mapa {
-            height: 500px; 
-            min-width: 310px; 
-            max-width: 800px; 
-            margin: 0 auto; 
+            height: 85vh;
+            min-height: 400px;
+            width: 100%;
+            min-width: 200px;
+            margin: 0 auto;
         }
         .loading {
             margin-top: 10em;
@@ -46,76 +47,11 @@
                             <div class="layer w-100">
                                 <!-- Progress Bars -->
                                 <div class="layers mB-15" id="progress-layer">
-                                    <div class="layer w-100 mT-15 c-grey-900">
+                                    <div class="layer w-100 mT-20 c-grey-900">
                                         <center>
                                             <i class="fa fa-circle-o-notch fa-spin fa-5x"></i>
                                         </center>
                                     </div>
-
-                                    {{-- <div class="layer w-100 mT-15">
-                                        <h5 class="mB-5">Alcaldes en Municipios Legales</h5>
-                                        <small class="fsz-sm fw-400 c-grey-700" id="alcaldes-legal-titulo">
-                                            {{ $e->alcaldesLegales }} de {{ $e->municipiosLegales }}
-                                        </small>
-                                        <span class="pull-right c-grey-600 fsz-sm" id="alcaldes-legal-per">
-                                            {{ $e->alcaldesLegales_per }}%
-                                        </span>
-                                        <div class="progress mT-10">
-                                            <div class="progress-bar bgc-blue-700" role="progressbar"
-                                            aria-valuenow="{{ $e->alcaldesLegales_per }}"
-                                            aria-valuemin="0" aria-valuemax="100"
-                                            style="width:{{ $e->alcaldesLegales_per }}%;"
-                                            id="alcaldes-legal-bar"></div>
-                                        </div>
-                                    </div>
-                                    <div class="layer w-100">
-                                        <h5 class="mB-5">Alcaldes en Municipios No Legales</h5>
-                                        <small class="fsz-sm fw-400 c-grey-700" id="alcaldes-no-legal-titulo">
-                                            {{ $e->alcaldesNoLegales }} de {{ $e->municipiosNoLegales }}
-                                        </small>
-                                        <span class="pull-right c-grey-600 fsz-sm" id="alcaldes-no-legal-per">
-                                            {{ $e->alcaldesNoLegales_per }}%
-                                        </span>
-                                        <div class="progress mT-10">
-                                            <div class="progress-bar bgc-brown-300" role="progressbar"
-                                            aria-valuemin="0" aria-valuemax="100"
-                                            aria-valuenow="{{ $e->alcaldesNoLegales_per }}"
-                                            style="width:{{ $e->alcaldesNoLegales_per }}%;"
-                                            id="alcaldes-no-legal-bar"></div>
-                                        </div>
-                                    </div>
-                                    <div class="layer w-100">
-                                        <h5 class="mB-5">Alcaldes General</h5>
-                                        <small class="fsz-sm fw-400 c-grey-700" id="alcaldes-titulo">
-                                            {{ $e->alcaldes }} de {{ $e->municipios }}
-                                        </small>
-                                        <span class="pull-right c-grey-600 fsz-sm" id="alcaldes-per">
-                                            {{ $e->alcaldes_per }}%
-                                        </span>
-                                        <div class="progress mT-10">
-                                            <div class="progress-bar bgc-deep-purple-500" role="progressbar"
-                                            aria-valuemin="0" aria-valuemax="100"
-                                            aria-valuenow="{{ $e->alcaldes_per }}"
-                                            style="width:{{ $e->alcaldes_per }}%;"
-                                            id="alcaldes-bar"></div>
-                                        </div>
-                                    </div>
-                                    <div class="layer w-100 mT-15">
-                                        <h5 class="mB-5">Municipios Legales</h5>
-                                        <small class="fsz-sm fw-400 c-grey-700" id="muni-titulo">
-                                            {{ $e->municipiosLegales }} de {{ $e->municipios }}
-                                        </small>
-                                        <span class="pull-right c-grey-600 fsz-sm" id="muni-per">
-                                            {{ $e->municipiosLegales_per }}%
-                                        </span>
-                                        <div class="progress mT-10">
-                                            <div class="progress-bar bgc-green-500" role="progressbar"
-                                            aria-valuenow="{{ $e->municipiosLegales_per }}"
-                                            aria-valuemin="0" aria-valuemax="100"
-                                            style="width:{{ $e->municipiosLegales_per }}%;"
-                                            id="muni-bar"></div>
-                                        </div>
-                                    </div> --}}
                                 </div>
 
                                 <!-- Pie Charts -->
@@ -156,12 +92,6 @@
     <script src={{ asset('js/highcharts/exporting.js') }}></script>
     <script src={{ asset('js/highcharts/data.js') }}></script>
     <script src={{ asset('js/highcharts/drilldown.js') }}></script>
-    
-	<!-- <script src=https://code.highcharts.com/maps/modules/exporting.js></script> -->
-	{{-- <script src=https://code.highcharts.com/maps/modules/data.js></script> --}}
-	{{-- <script src=https://code.highcharts.com/maps/modules/drilldown.js></script> --}}
-        
-    {{-- <script src="{{ asset('js/highcharts/gt-all.geo.json') }}"></script> --}}
 
     <script src="{{ asset('js/highcharts/gt-all.js') }}"></script>
 
@@ -178,6 +108,24 @@
                 $("#mapa").highcharts().showLoading('<i class="fa fa-spinner fa-spin fa-3x"></i>');
                 Mapainit(tipo);
             });
+            
+            $(window).resize(function() {
+                clearTimeout(this.id);
+                this.id = setTimeout(doneResizing, 500);
+            });
+
+            function doneResizing(){
+                const mapa = $("#mapa").highcharts();
+                let width = 0;
+                if ($(document).width() < 1200){
+                    width = $("#mapa").closest('.peers').width() - 25;
+                } else {
+                    width = $("#mapa").width() - 25;
+                }
+                if(mapa){
+                    mapa.setSize(width, null, false);
+                }
+            }
         });
     </script>
     
